e084f0808f9e89f6d375e7a20ea6708cff83528f
giaaaangi <64853899+giaaaangi@users.noreply.github.com>
2023-06-14 20:57:48 +0200

    Outline

--- Assets/Scripts/Wisps/Wisp.cs
+++ Assets/Scripts/Wisps/Wisp.cs
@@ -6,10 +6,9 @@ using UnityEngine.AI;
 public class Wisp : MonoBehaviour
 {
     [SerializeField] Transform followPoint;
-    [SerializeField] WispAgent agent;
     [SerializeField] float floatingFrequency = 1f;
     [SerializeField] float floatingAmount = 1f;
-
+    [SerializeField] Transform mesh;
     public bool isInTotem;
 
     private Transform agentTransform;
@@ -17,27 +16,41 @@ public class Wisp : MonoBehaviour
 
     private float seed;
     private float floatingOffset;
+    //private void Awake()
+    //{
+    //    agentTransform = agent.transform;
+    //    agentTransform.parent = transform.parent;
+    //    Vector3 position = agentTransform.position;
+    //    //position.y = 0;
+    //    agentTransform.position = position;
+    //    agent.FollowPoint = followPoint;
+    //    height = transform.position.y;
+
+    //    seed = Random.Range(0, Mathf.PI * 2);
+    //}
+
+    //private void Update()
+    //{
+    //    
+
+    //}
+    private NavMeshAgent agent;
     private void Awake()
     {
-        agentTransform = agent.transform;
-        agentTransform.parent = transform.parent;
-        Vector3 position = agentTransform.position;
-        //position.y = 0;
-        agentTransform.position = position;
-        agent.FollowPoint = followPoint;
-        height = transform.position.y;
-
-        seed = Random.Range(0, Mathf.PI * 2);
+        agent = GetComponent<NavMeshAgent>();
+        height = mesh.position.y;
     }
 
     private void Update()
     {
+        agent.SetDestination(followPoint.position);
+
         floatingOffset += Time.deltaTime;
 
-        Vector3 position = agentTransform.position;
+
+        Vector3 position = mesh.position;
         position.y = height + Mathf.Sin((seed + floatingOffset) * floatingFrequency) * floatingAmount;
-        transform.position = position;
-        
+        mesh.position = position;
     }
 
     public void GoToTotem(Totem totem)
