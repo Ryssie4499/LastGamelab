9d02cf8ae9cc66832ba862f20856389c6e5d70b9
Beatrice Sipos <sipibea2001@gmail.com>
2023-06-15 09:37:11 +0200

    Movimento orbitale dei whisps

--- Assets/Scripts/Wisps/Wisp.cs
+++ Assets/Scripts/Wisps/Wisp.cs
@@ -8,8 +8,14 @@ public class Wisp : MonoBehaviour
     [SerializeField] Transform followPoint;
     [SerializeField] float floatingFrequency = 1f;
     [SerializeField] float floatingAmount = 1f;
+
+    [SerializeField] private float degreesPerSecond = 45;
+
     [SerializeField] Transform mesh;
     public bool isInTotem;
+
+    public GameObject Player;
+    public bool hasStopped;
 
     private Transform agentTransform;
     private float height;
@@ -35,26 +41,59 @@ public class Wisp : MonoBehaviour
 
     //}
     private NavMeshAgent agent;
+    Rigidbody playerRB;
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        playerRB = Player.GetComponent<Rigidbody>();
         height = mesh.position.y;
     }
 
     private void Update()
     {
-        agent.SetDestination(followPoint.position);
-
         floatingOffset += Time.deltaTime;
 
 
         Vector3 position = mesh.position;
         position.y = height + Mathf.Sin((seed + floatingOffset) * floatingFrequency) * floatingAmount;
         mesh.position = position;
+
+        //se il player non si muove (non prendeva 0 perchè si muoveva sempre di un millimetro senza che toccassi niente) fa partire il timer di 5 secondi
+        if (playerRB.velocity.magnitude >= 0 && playerRB.velocity.magnitude <= 0.1f)
+        {
+            StartCoroutine(TimeToOrbitate());
+        }
+        else if (playerRB.velocity.magnitude > 0.1f)
+        {
+            hasStopped = false;
+        }
+
+        //se si muove prende la tua destinazione
+        if (!hasStopped)
+        {
+            agent.SetDestination(followPoint.position);
+
+        }
+        //se non si muove orbita
+        else
+            Orbitate();
     }
 
     public void GoToTotem(Totem totem)
     {
 
+    }
+    //gli resetto la destinazione e lo faccio ruotare attorno al player sull'asse y di 45° al secondo
+    public void Orbitate()
+    {
+        agent.ResetPath();
+        agent.gameObject.transform.RotateAround(Player.transform.position, Vector3.up, degreesPerSecond * Time.deltaTime);
+    }
+
+    //timer di cinque secondi, dopo quel lasso di tempo, si rende conto di essere fermo da troppo
+    IEnumerator TimeToOrbitate()
+    {
+        yield return new WaitForSeconds(5);
+        hasStopped = true;
     }
 }
